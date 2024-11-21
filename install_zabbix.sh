2c0ab8dbdc8dccfef30bb9abd591150c4b037dc3
akuliksilasveta <a.kulik@silasveta.com>
2024-11-21 15:11:05 +0300

    Update install_zabbix.sh

--- install_zabbix.sh
+++ install_zabbix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 ZABBIX_SERVER="zabbix.silasveta.local"
+ZABBIX_SERVER2="zabbix.silasveta.local,zabbix1.silasveta.local,zabbix2.silasveta.local,zabbix3.silasveta.local"
 HOSTNAME="hostname"
 
 if [ "$UID" -ne 0 ]; then
@@ -19,7 +20,7 @@ if [ -x /usr/bin/apt-get ]; then
         apt-get install zabbix-agent
 
         sed -i "/^Server=/c\Server=${ZABBIX_SERVER}" /etc/zabbix/zabbix_agentd.conf
-        sed -i "/^ServerActive=/c\ServerActive=${ZABBIX_SERVER}" /etc/zabbix/zabbix_agentd.conf
+        sed -i "/^ServerActive=/c\ServerActive=${ZABBIX_SERVER2}" /etc/zabbix/zabbix_agentd.conf
         sed -i "/^Hostname=/c\Hostname=${HOSTNAME}" /etc/zabbix/zabbix_agentd.conf
 
         sed -i "s/# StartAgents=3/StartAgents=5/;
@@ -37,7 +38,7 @@ if [ -x /usr/bin/yum ]; then
           yum -y install zabbix-agent
           chkconfig zabbix-agent on
           sed -i "/^Server=/c\Server=${ZABBIX_SERVER}" /etc/zabbix/zabbix_agentd.conf
-          sed -i "/^ServerActive=/c\ServerActive=${ZABBIX_SERVER}" /etc/zabbix/zabbix_agentd.conf
+          sed -i "/^ServerActive=/c\ServerActive=${ZABBIX_SERVER2}" /etc/zabbix/zabbix_agentd.conf
           sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
           service zabbix-agent restart
 fi
